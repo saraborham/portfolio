75fa61c538c1ca1a4bdd74991e2d3b73b8387e64
Sara Borham <saraborham193@yahoo.com>
2019-04-13 23:38:51 +0200

    Updates

---
+++ precache-manifest.108f2cc0fd54c87dcbaefd754c3709cc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "302bacdc0786bda67b9d5c8c289ba688",
+    "url": "/portfolio/static/media/arrow1.302bacdc.svg"
+  },
+  {
+    "revision": "a5e24b9c8607e6d67273",
+    "url": "/portfolio/static/css/main.2ac67e11.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "69ff2c2e502b9579f5ac",
+    "url": "/portfolio/static/js/2.686c13c3.chunk.js"
+  },
+  {
+    "revision": "3870e23fbba209ece375bd535eef2014",
+    "url": "/portfolio/static/media/photography.3870e23f.jpg"
+  },
+  {
+    "revision": "c1f1bcfc0e731bdacb425ab61fe7f5e9",
+    "url": "/portfolio/static/media/huddle.c1f1bcfc.jpg"
+  },
+  {
+    "revision": "db54652d1249e95814c45bdbee9a24ce",
+    "url": "/portfolio/static/media/design-half.db54652d.png"
+  },
+  {
+    "revision": "6ce875eb1e77692553d2b453969fea7a",
+    "url": "/portfolio/static/media/code-half.6ce875eb.png"
+  },
+  {
+    "revision": "a5e24b9c8607e6d67273",
+    "url": "/portfolio/static/js/main.9dab5748.chunk.js"
+  },
+  {
+    "revision": "f1b4d80a2a5596a7622112a36d736e0a",
+    "url": "/portfolio/static/media/linkedin.f1b4d80a.svg"
+  },
+  {
+    "revision": "63681da54f468f0c31d70dbb9df95d5d",
+    "url": "/portfolio/static/media/facebook.63681da5.svg"
+  },
+  {
+    "revision": "e507962f02672db115cba55a8a23faec",
+    "url": "/portfolio/static/media/behance.e507962f.svg"
+  },
+  {
+    "revision": "0cd0691c1fef848f4a90d5cde6b751ca",
+    "url": "/portfolio/static/media/mail.0cd0691c.svg"
+  },
+  {
+    "revision": "c915644f011872c0d8e40594bcf9a502",
+    "url": "/portfolio/static/media/twitter.c915644f.svg"
+  },
+  {
+    "revision": "9608715da15bb1017a0161d25068affe",
+    "url": "/portfolio/static/media/bg2.9608715d.svg"
+  },
+  {
+    "revision": "cf3df5d2f8082255799ea616f8009f8e",
+    "url": "/portfolio/static/media/mylogo.cf3df5d2.svg"
+  },
+  {
+    "revision": "b84347c032f36554d213d32fe70c451b",
+    "url": "/portfolio/index.html"
+  }
+];
